914a2ec500809eb134793f4f09133bb99fce78a1
clawoo <alin.claudiu.radut@gmail.com>
2011-08-02 19:55:07 +0300

    Initial check-in

---
+++ android/build_native.sh
@@ -0,0 +1,29 @@
+# set params
+ANDROID_NDK_ROOT=/Users/clw/Development/AndroidNDK
+COCOS2DX_ROOT=/Users/clw/Projects/Cocos2d-x
+GAME_ROOT=$COCOS2DX_ROOT/SimpleGame
+GAME_ANDROID_ROOT=$GAME_ROOT/android
+RESOURCE_ROOT=$GAME_ROOT/Resource
+
+# make sure assets is exist
+if [ -d $GAME_ANDROID_ROOT/assets ]; then
+    rm -rf $GAME_ANDROID_ROOT/assets
+fi
+
+mkdir $GAME_ANDROID_ROOT/assets
+
+# copy resources
+for file in $RESOURCE_ROOT/*
+do
+    if [ -d $file ]; then
+        cp -rf $file $GAME_ANDROID_ROOT/assets
+    fi
+
+    if [ -f $file ]; then
+        cp $file $GAME_ANDROID_ROOT/assets
+    fi
+done
+
+# build
+$ANDROID_NDK_ROOT/ndk-build -C $GAME_ANDROID_ROOT $*
+
