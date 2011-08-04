30e274a298f3fc64c9729e010efd791b38acfcd9
clawoo <alin.claudiu.radut@gmail.com>
2011-08-04 19:01:05 +0300

    Remove obj and libs folders

--- android/obj/local/armeabi/objs-debug/cocos2d/support/ccUtils.o.d
+++
@@ -1,5 +0,0 @@
-/Users/clw/Projects/Cocos2d-x/SimpleGame/android/obj/local/armeabi/objs-debug/cocos2d/support/ccUtils.o: \
- /Users/clw/Projects/Cocos2d-x/SimpleGame/android/jni/../../../cocos2dx/support/ccUtils.cpp \
- /Users/clw/Projects/Cocos2d-x/SimpleGame/android/jni/../../../cocos2dx/support/ccUtils.h
-
-/Users/clw/Projects/Cocos2d-x/SimpleGame/android/jni/../../../cocos2dx/support/ccUtils.h:
